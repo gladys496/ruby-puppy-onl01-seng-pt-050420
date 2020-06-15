4a9bf61ddc9d5ac447a5954c1ae92d6dd63b2490
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-06-15 06:31:16 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,27 +1,3 @@
-class Dog 
-  attr_reader :name 
-  
-  @@all = []
-  
-   def initialize(name)
-    @name = name
-    @@all << self
-  end
- 
-  def self.all
-    @@all
-  end
-  def self.clear_all
-    @@all.clear
-  end 
-  def self.print_all
-        @@all.each do |x|
-            return x.name 
-          end 
-        end 
-  
- 
-end 
 
 class Dog
     attr_reader :name
